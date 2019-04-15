2e85776bb7925188d74325e46a17aa0b15bfc0f8
ndleyton <nicoleytongo@gmail.com>
2019-04-15 05:21:44 -0400

    fixed rust dependencies and compiled C into wasm

--- subset-sum-0/src/lib.rs
+++ subset-sum-0/src/lib.rs
@@ -12,12 +12,25 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[macro_use]
 extern crate serde_derive;
 
-#[test]c
+fn list_combinations(list: &[i64], current: i64) -> Vec<i64> {
+	let mut results = Vec::new();
+	for (index, &int) in list.iter().enumerate() {
+		results.push(current + int);
+
+		if (list.len() - index) > 1 {
+			results.extend(list_combinations(&list[index+1..list.len()], current + int));
+		}
+	}
+	return results;
+}
+
 #[wasm_bindgen]
 pub fn subset_sum_r(array: JsValue){
-    let numbers: Vec<u32> = array.into_serde().unwrap();
-    const A: u32 = numbers.iter().filter(|x| x.is_positive()).sum();
-    const B: u32 = numbers.iter().filter(|x| x.is_negative()).sum();
+    let numbers: Vec<i32> = array.into_serde().unwrap();
+    const A: i32 = numbers.iter().filter(|x| x.is_positive()).sum();
+    const B: i32 = numbers.iter().filter(|x| x.is_negative()).sum();
     assert!(A.is_positive());
     assert!(B.is_negative());
 }
+
+
