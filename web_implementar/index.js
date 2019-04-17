4baf7d0ad9beb1929cbe292ea3a038513df7e04b
Andre Simon <aasimon@uc.cl>
2019-04-17 08:57:16 -0400

    working subset sum 0 ui

--- web_implementar/index.js
+++ web_implementar/index.js
@@ -1,4 +1,30 @@
 import * as wasm from "@ndleyton/subset-sum-0";
 
-const array = [2,4];
-console.log(wasm.subset_sum(array, 0));
+const updateRepresentation = (array, id) => {
+  document.getElementById(id).innerHTML = "[" + array.toString() + "]";
+};
+
+const main = () => {
+  let array = [];
+
+  document.getElementById("addElementButton").onclick = function(e) {
+    const numberInput = document.getElementById("addNumberInput");
+    const rawValue = numberInput.value;
+    array.push(parseInt(rawValue, 10));
+    updateRepresentation(array, "array");
+    numberInput.value = "";
+  };
+
+  document.getElementById("testArrayButton").onclick = function(e) {
+    const res = wasm.subset_sum(array, 0);
+    document.getElementById("result").innerHTML = res;
+  };
+
+  document.getElementById("clearArrayButton").onclick = function(e) {
+    array = [];
+    updateRepresentation(array, "array");
+    document.getElementById("result").innerHTML = "...";
+  };
+};
+
+main();
